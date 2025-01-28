c46e54c2e67b5e80b9a63aa52e2900fba808c7b1
Maksym Kravchuk <maksimkravcuk14@gmail.com>
2025-01-28 15:08:20 +0200

    Updated user serializer

--- core/user/serializers.py
+++ core/user/serializers.py
@@ -25,9 +25,7 @@ class UserSerializer(AbstractSerializer):
         representation = super().to_representation(instance)
         if not representation['avatar']:
             representation['avatar'] = settings.DEFAULT_AVATAR_URL.format(instance.name)
-            return representation
-
-        if settings.DEBUG:  # debug enabled for dev
+        else:
             base_url = getattr(settings, 'BASE_URL', 'http://localhost:8000')
             representation["avatar"] = urljoin(base_url, representation["avatar"])
         return representation
